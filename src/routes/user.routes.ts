10a24c63682044ced4653adc1d6b2b3a8e76a1b4
Ozcar Clemente <163335710+ozcarclemente@users.noreply.github.com>
2025-10-08 10:33:20 -0600

    Fix user.routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
 import { getUserById } from "./../controller/user.controller";
-import { authMiddleware } from "./../middleware/auth.middleware";
-
 
 const router = Router()
 
-router.get('/:id', authMiddleware, getUserById)
+router.get('/:id', getUserById)
 
 export default router
